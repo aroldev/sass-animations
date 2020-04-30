8d26f4a9745eaa0602094ebbad4cded227310b42
Arol <arol.php@gmail.com>
2020-04-30 17:06:53 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# sass-animations
+# Sass Animations
 A collection of animations
+
+What animations you can find here:
+
+* Blur animation
+* Circle flash effect
+* Decolorize CSS animation
+* Flash (Material Design animation)
+* Scale
+* Shine hover effect
+
+## Purpose
+The main purpose of this collection of animation is to include in your project only the animation that you need.
+
+You can keep all these files in your development environment and comment in animation_manager these that you don't need it to avoid compiling in your CSS.
+
+#### Credits
+[dev.arol.me](https://dev.arol.me) 
---
+++ animations.php
@@ -0,0 +1,38 @@
+<div class="row">
+    <div class="col-xs-6">
+        <h4>Shine animation</h4>
+        <a href="#" class="shine-animation">
+            <img src="https://picsum.photos/400/300?random=1" alt="Shine effect">
+        </a>
+    </div>
+    <div class="col-xs-6">
+        <h4>Circle animation</h4>
+        <a href="#" class="circle-animation">
+            <img src="https://picsum.photos/400/300?random=2" alt="Circle effect">
+        </a>
+    </div>
+    <div class="col-xs-6">
+        <h4>Flash animation</h4>
+        <a href="#" class="flash-animation">
+            <img src="https://picsum.photos/400/300?random=3" alt="Flash effect">
+        </a>
+    </div>
+    <div class="col-xs-6">
+        <h4>Scale animation</h4>
+        <a href="#" class="scale-animation">
+            <img src="https://picsum.photos/400/300?random=4" alt="Scale effect">
+        </a>
+    </div>
+    <div class="col-xs-6">
+        <h4>Decolor animation</h4>
+        <a href="#" class="decolor-animation">
+            <img src="https://picsum.photos/400/300?random=5" alt="Decolor effect">
+        </a>
+    </div>
+    <div class="col-xs-6">
+        <h4>Blur animation</h4>
+        <a href="#" class="blur-animation">
+            <img src="https://picsum.photos/400/300?random=6" alt="Blur effect">
+        </a>
+    </div>
+</div>
